8fe87347e7ec8d7cd48bfb67695c8f8459b74c74
Mohammad Hamza <mhdkh444@gmail.com>
2024-12-16 11:42:46 +0530

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,48 +13,48 @@ const projects = [
     "description": "A full-stack URL shortener app with click tracking, user location, device information, and the ability to create custom URLs, providing detailed analytics on link interactions.",
     "image": port1,
     "tech": ["React", "Node.js", "MongoDB", "Express"],
-    "github": "#",
-    "live": "#"
+    "github": "https://github.com/mh-hamza/URL-SHORTNER-MERN-APP",
+    "live": "https://mh-shrink.netlify.app/"
   },
   {
     "title": "Ayush Pathology Lab",
     "description": "A collaborative task management tool with real-time updates and an admin panel for CRUD operations on reports. Also includes an appointment system for clients.",
     "image": port2,
     "tech": ["React", "Firebase", "Tailwind CSS"],
-    "github": "#",
-    "live": "#"
+    "github": "https://github.com/mh-hamza/Ayush-Pathology-Demo",
+    "live": "https://ayush-pathology-demo.netlify.app/"
   },
   {
     "title": "College Web",
     "description":  "A responsive website for the college with real-time features, an admin panel for managing school data, and an online admission system, built using React and Firebase.",
     "image": port3,
     "tech": ["React", "Firebase"],
-    "github": "#",
-    "live": "#"
+    "github": "https://sgsic-demo.netlify.app/",
+    "live": "https://sgsic-demo.netlify.app/"
   },
   {
     "title": "Space Web",
     "description": "UI-only model of a space-themed website showcasing modern design with AOS animations, without any functional backend.",
     "image": port4,
     "tech": ["React", "Tailwind CSS", "AOS animation"],
-    "github": "#",
-    "live": "#"
+    "github": "https://github.com/mh-hamza/SPACE-WEBSITE-React",
+    "live": "https://mh-space-tech.netlify.app/"
   },
   {
     "title": "Task Manager Todo",
     "description": "A task management app with real-time updates, built with React and Firebase, allowing users to manage tasks efficiently.",
     "image": port5,
     "tech": ["React", "Firebase"],
-    "github": "#",
-    "live": "#"
+    "github": "https://github.com/mh-hamza/TASK-MANAGER",
+    "live": "https://task-manager-project-work.netlify.app/"
   },
   {
     "title": "Old Portfolio",
     "description": "A personal, fully responsive portfolio website with smooth AOS animations with a modern design.",
     "image": port6,
     "tech": ["HTML", "CSS", "JavaScript", "AOS animation"],
-    "github": "#",
-    "live": "#"
+    "github": "https://github.com/mh-hamza/My-Personal-Portfolio",
+    "live": "https://mhhamza.netlify.app/"
   }
 ];
 
